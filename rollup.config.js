8541fff8e2b2bc9e1c19b493c7289b80ae6e216c
Maciej Krawczyk <git@kravchyk.com>
2022-07-25 06:11:32 +0200

    fix: Exclude /test directory from TypeScript when building
    This fixes an odd issue where TypeScript and rollup-plugin-typescript2
    output type declarations for all files like dist/src and dist/test and
    omit the actual root declaration file that's required.

--- rollup.config.js
+++ rollup.config.js
@@ -69,6 +69,7 @@ export default [
             declaration: true,
             declarationDir: './dist',
           },
+          exclude: ['./test'],
         },
         useTsconfigDeclarationDir: true,
       }),
